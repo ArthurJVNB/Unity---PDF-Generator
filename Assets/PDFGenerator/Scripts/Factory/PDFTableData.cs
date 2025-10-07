91c1996d577098fb754d6cb78cedc65ecfe73885
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-10-07 09:55:28 -0300

    Implementing PDFTableData

---
+++ Assets/PDFGenerator/Scripts/Factory/PDFTableData.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Project.PDFGenerator
+{
+	[Serializable]
+	public class PDFTableData : BasePDFData
+	{
+		public List<string> header = new();
+		public List<List<string>> rows = new();
+		//TODO: style
+		//TODO: headerStyle
+		//TODO: rowStyle
+
+		public PDFTableData(PDFJObjectFactory factory) : base(factory)
+		{
+			type = PDFConstants.k_TableType;
+		}
+
+		public PDFTableData SetHeader(List<string> header)
+		{
+			this.header = header;
+			return this;
+		}
+
+		public PDFTableData SetHeader(params string[] header)
+		{
+			return SetHeader(new List<string>(header));
+		}
+
+		public PDFTableData AddRow(List<string> row)
+		{
+			rows.Add(row);
+			return this;
+		}
+
+		public PDFTableData AddRow(params string[] row)
+		{
+			return AddRow(new List<string>(row));
+		}
+
+		//TODO: Export (JObject)
+	}
+}
