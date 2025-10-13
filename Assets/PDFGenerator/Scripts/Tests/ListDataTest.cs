d2a55919e3500964e4e0a9e4acb06f4384ac7560
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-10-13 10:02:46 -0300

    Implemented ListStyleType, and updated list style (ListStyleData) and test (ListDataTest)

--- Assets/PDFGenerator/Scripts/Tests/ListDataTest.cs
+++ Assets/PDFGenerator/Scripts/Tests/ListDataTest.cs
@@ -10,9 +10,11 @@ namespace Project.PDFGenerator.Tests
 			const string expected = "{\"type\":\"list\",\"ordered\":false,\"items\":[\"Item 1\",\"Item 2\",\"Item 3\"]," +
 				"\"style\":{\"font-size\":\"13px\",\"line-height\":\"1.6\",\"color\":\"#212F3C\",\"margin-left\":\"25px\",\"list-style-type\":\"disc\"}}";
 			var actual = new PDFListData(null).SetOrdered(false).AddItem("Item 1").AddItem("Item 2").AddItem("Item 3")
-				.AddStyle(13).SetColor(PDFColorUtility.ParseHtmlString("#212F3C")).SetMarginLeft(25).SetLineHeight(1.6f).DoneStyle()
+				.AddStyle(13).SetColor(PDFColorUtility.ParseHtmlString("#212F3C")).SetMarginLeft(25).SetLineHeight(1.6f).SetListStyleType(ListStyleType.Disc).DoneStyle()
 				.GetExportData().ToString(Newtonsoft.Json.Formatting.None);
 			Assert.AreEqual(expected, actual, "Expected: {0}\n  Received: {1}", expected, actual);
 		}
+
+		//TODO: test all styles of ListStyleData
 	}
 }
