f702f220cb49d70f73a401a3292cfc45967ccde4
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-10-18 20:15:55 -0300

    Release v1.0.0

--- Assets/PDFGenerator/Scripts/Runtime/PDFConstants.cs
+++ Assets/PDFGenerator/Scripts/Runtime/PDFConstants.cs
@@ -1,14 +1,14 @@
-using UnityEngine;
-
 namespace Project.PDFGenerator
 {
-    public static class PDFConstants
-    {
-        public const string k_TextType = "text";
-        public const string k_ImageType = "image";
-        public const string k_ListType = "list";
-        public const string k_TableType = "table";
+	public static class PDFConstants
+	{
+		public const string k_Version = "1.0.0";
+
+		public const string k_TextType = "text";
+		public const string k_ImageType = "image";
+		public const string k_ListType = "list";
+		public const string k_TableType = "table";
 		public const string k_Pixel = "px";
-        public const int k_DefaultFontSize = 12;
+		public const int k_DefaultFontSize = 12;
 	}
 }
